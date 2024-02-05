d5d104be40ecb985cb5338a4ce4c5d238cecec00
mustafa Tariq <111755895+Mustafa-tariq23@users.noreply.github.com>
2024-02-05 21:51:28 +0500

    Career section final

---
+++ src/components/HeaderForPages.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const HeaderForPages = (props) => {
+  const { heading, para } = props
+  return (
+    <>
+      <Navbar />
+      <div className='bg-indigo-500 h-[25rem] flex flex-col justify-end items-center  pb-12 gap-4'>
+        <h1 className='text-5xl font-bold flex flex-wrap text-white'>
+          {heading}
+        </h1>
+        <p className='w-[50%] text-center p-4 text-gray-200'>
+          {para}
+        </p>
+      </div>
+      <svg className="fill-indigo-500 bg-gray-100" viewBox="0 0 1440 57" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1440 0H0V57C720 0 1440 57 1440 57V0Z"></path>
+      </svg>
+    </>
+  )
+}
+
+export default HeaderForPages
