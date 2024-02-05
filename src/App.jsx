39f00d596149d97a81aba7c1d42019b7220e05b0
mustafa Tariq <111755895+Mustafa-tariq23@users.noreply.github.com>
2024-02-05 21:42:56 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Services from './components/Services'
+import Career from './components/Career'
+import './index.css';
+import { Routes, Route } from 'react-router-dom';
+import Form from './components/Form';
+
+function App() { 
+
+  return (
+    <>
+    <Routes>
+      <Route  path="/" element={<Home/>}/>
+      <Route  path="/AboutUs" element={<About/>}/>
+      <Route  path="/Services" element={<Services/>}/>
+      <Route  path="/Career" element={<Career/>}/>
+      <Route  path="/ContactUs" element={<Contact/>}/>
+      <Route  path="/ContactUs/Form" element={<Form/>}/>
+    </Routes>
+
+    </>
+  );
+}
+
+export default App;
