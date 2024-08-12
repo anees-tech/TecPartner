3f238e2174207dadc913dcfb93407c0d009acade
Mustafa Tariq <mt2032303@gmail.com>
2024-08-12 18:52:17 +0500

    favicon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Services from './components/Services'
-import Career from './components/Career'
-import './index.css';
-import { Routes, Route } from 'react-router-dom';
-import Form from './components/Form';
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
+import Career from "./components/Career";
+import "./index.css";
+import { Routes, Route } from "react-router-dom";
+import Form from "./components/Form";
 
-function App() { 
-
+function App() {
   return (
     <>
-    <Routes>
-      <Route  path="/" element={<Home/>}/>
-      <Route  path="/AboutUs" element={<About/>}/>
-      <Route  path="/Services" element={<Services/>}/>
-      <Route  path="/Career" element={<Career/>}/>
-      <Route  path="/ContactUs" element={<Contact/>}/>
-      <Route  path="/ContactUs/Form" element={<Form/>}/>
-    </Routes>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/AboutUs" element={<About />} />
+        <Route path="/Services" element={<Services />} />
+        <Route path="/Career" element={<Career />} />
+        <Route path="/ContactUs" element={<Contact />} />
+        <Route path="/ContactUs/Form" element={<Form />} />
+      </Routes>
     </>
   );
 }
